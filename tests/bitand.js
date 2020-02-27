e1c2ae43cec1f19fe7f6adb5143468acb90751de
Azazel <61528171+azazelnunca@users.noreply.github.com>
2020-02-26 23:40:19 -0300

    Creating a example handle

---
+++ tests/bitand.js
@@ -0,0 +1,17 @@
+const { sum, floor, mod, round, pow, log } = require("./src/Lib/Equation");
+// disrupting the wrapper
+
+const bitand = (x, y) => {
+    let and = 0;
+    // creating a variable "and" to return the result
+    sum(floor(log(x)), (kx) => {
+        // floor the log of x
+        // arrow function with one parameter "kx"
+        and += pow(2, kx) * round(mod(floor(x / pow(2, kx)), 2)) * round(mod(floor(y / pow(2, kx)), 2));
+        // adding the formula
+    }, 0);
+    // end of sum
+    return and;
+    // return the output
+}
+// x & y
